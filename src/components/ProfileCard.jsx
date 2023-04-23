4d869fc30e7b0097a021a6d6c84964ef977342e6
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-23 21:46:37 +0530

    intial commit

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import profileData from "../data/profile.json";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import Image from "next/image";
+
+export default function ProfileCard() {
+  return (
+    <section className="p-5">
+      {/*  To show cards grid  */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  ">
+        {profileData.map((profile, index) => (
+          //  Profile Card
+          <div
+            key={index}
+            class="flex  flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
+            <Image
+              class="ml-3 rounded-md w-32 mt-5 md:mt-0 "
+              src={`https://github.com/${profile.GitHubUserName}.png`}
+              width={500}
+              height={500}
+              alt={profile.GitHubUserName}
+            />
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <p class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {profile.GitHubUserName}
+              </p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {profile.bio}
+              </p>
+              <span className="flex gap-5">
+                <a
+                  href={`https://github.com/${profile.GitHubUserName}`}
+                  target="_blank"
+                >
+                  {" "}
+                  <FaGithub size={30} />
+                </a>
+                <a
+                  href={`https://twitter.com/${profile.TwitterUserName}`}
+                  target="_blank"
+                >
+                  {" "}
+                  <FaTwitter size={30} />
+                </a>
+                <a
+                  href={`https://linkedin.com/in/${profile.LinkedInUserName}`}
+                  target="_blank"
+                >
+                  {" "}
+                  <FaLinkedin size={30} />
+                </a>
+              </span>
+              <p className="pt-2">{profile.occupation}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
